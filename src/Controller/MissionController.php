fea97b44ff725f8ff54db021d11043c164d9d1cd
Lalaina RAJAONAHSOA <r.lalainafr@gmail.com>
2022-04-25 14:42:40 +0200

    Mission CRUD

--- src/Controller/MissionController.php
+++ src/Controller/MissionController.php
@@ -4,15 +4,18 @@ namespace App\Controller;
 
 use App\Entity\Mission;
 use App\Form\MissionType;
+use App\Form\MissionChoiceType;
 use App\Repository\MissionRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MissionController extends AbstractController
 {
+
     /**
      * @Route("/mission", name="app_mission")
      */
@@ -56,30 +59,45 @@ class MissionController extends AbstractController
         ]);
     }
     /**
-     * @Route("/mission/edit/{id}", name="app_mission_edit")
+     * @Route("/mission/edit", name="app_mission_edit")
      */
-    public function edit($id, MissionRepository $missionRepository, Request $request, EntityManagerInterface $em): Response
+    public function edit(MissionRepository $missionRepository, Request $request, EntityManagerInterface $em): Response
     {
+        $searchForm = $this->createForm(MissionChoiceType::class);
+        $searchForm->handleRequest($request);
+        $req = $request->request->get('mission_choice');
+        $id = $req['mission'];
         $mission = $missionRepository->findOneById($id);
-        $form = $this->createForm(MissionType::class, $mission);
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
+        $editForm = $this->createForm(MissionType::class, $mission);
+        $editForm->handleRequest($request);
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
             $em->flush();
+
             return $this->redirectToRoute('app_mission');
         }
-
         return $this->render('mission/edit.html.twig', [
-            'form' => $form->createView()
+            'searchForm' => $searchForm->createView(),
+            'editForm' => $editForm->createView(),
         ]);
     }
     /**
-     * @Route("/mission/delete/{id}", name="app_mission_delete")
+     * @Route("/mission/delete", name="app_mission_delete")
      */
-    public function delete($id, MissionRepository $missionRepository,  EntityManagerInterface $em): Response
+    public function delete(MissionRepository $missionRepository,  EntityManagerInterface $em, Request $request): Response
     {
-        $mission = $missionRepository->findOneById($id);
-        $em->remove($mission);
-        $em->flush();
-        return $this->redirectToRoute('app_mission');
+
+        $searchForm = $this->createForm(MissionChoiceType::class);
+        $searchForm->handleRequest($request);
+        if ($searchForm->isSubmitted() && $searchForm->isValid()) {
+            $mission = $searchForm->getData();
+            $missionId = $mission['mission']->getId();
+            $deleteMission = $missionRepository->findOneById($missionId);
+            $em->remove($deleteMission);
+            $em->flush();
+            return $this->redirectToRoute('app_mission');
+        }
+        return $this->render('mission/choice_delete.html.twig', [
+            'searcForm' => $searchForm->createView()
+        ]);
     }
 }
